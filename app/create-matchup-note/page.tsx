72f8f879221fb623347627e57a9abdfaf97c21bb
Madhu22 <madhuxsharma@gmail.com>
2025-04-24 01:23:31 -0400

    Plase no produciton error

--- app/create-matchup-note/page.tsx
+++ app/create-matchup-note/page.tsx
@@ -1,16 +1,15 @@
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { queryChampionNotes } from "@/actions/champion_notes";
 import { MatchupSelector } from "@/components/MatchupSelector";
 
-export default async function CreateMatchupNotePage({ searchParams }: { searchParams: { noteId?: string , championName?: string} }) {
+export default async function CreateMatchupNotePage({ params }: any) {
   const session = await auth.api.getSession({ headers: await headers() });
   if (!session) redirect("/auth/sign-in");
 
-  const championNoteId = searchParams.noteId;
-  if (!championNoteId) redirect("/champion-notes"); // fallback just in case
-  // Fetch champion list
+  const championNoteId = params.championId;
+  if (!championNoteId) redirect("/champion-notes");
+
   const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.8.1/data/en_US/champion.json");
   const data = await res.json();
   const champions = Object.values(data.data);
