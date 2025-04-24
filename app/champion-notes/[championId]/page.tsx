085de607c0f1742d4cf91d8e944ddef66ea19d01
Madhu22 <madhuxsharma@gmail.com>
2025-04-24 00:11:18 -0400

    force skip error message

--- app/champion-notes/[championId]/page.tsx
+++ app/champion-notes/[championId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { GeneralNotesEditor } from "@/components/GeneralNotesEditor";
 
+// @ts-expect-error
 export default async function ChampionNotePage({ params }: { params: { championId: Promise<string> } }) {
     const session = await auth.api.getSession({
         headers: await headers(),
